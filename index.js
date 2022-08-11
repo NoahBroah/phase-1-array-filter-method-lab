bb6bcc47436bd77b4fb1b4def9327d204fa96fde
Noah King <noahking123@yahoo.com>
2022-08-11 13:59:43 -0400

    commit tree 867ff3b3f2171f87b81e0b69b897475aaab217ff

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+
+
+/*function findMatching(collection, driver) {
+    const newArray = [];
+    for (const name of collection) {
+        if (name.toLowerCase() === driver.toLowerCase()) {
+            newArray.push(name);
+        }
+    }
+    return newArray;
+}
+*/
+// This is the same as the Function Above
+function findMatching(collection, driver) {
+    return collection.filter(name => name.toLowerCase() === driver.toLowerCase())
+}
+
+function fuzzyMatch(collection, driverLetter) {
+    return collection.filter(firstLetter => firstLetter[0] === driverLetter[0]);
+}
+
+function matchName(collection, driverString) {
+    return (collection.filter(driver => driver.name === driverString));
+}
